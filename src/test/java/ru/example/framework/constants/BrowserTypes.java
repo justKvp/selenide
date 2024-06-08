559768726640f6f50c2f305aa6f93381681cda63
kvipka <kvipka90@yandex.ru>
2024-06-08 20:56:02 +0300

    rework with new fixtures with @UsePlaywright annotation

--- src/test/java/ru/example/framework/constants/BrowserTypes.java
+++
@@ -1,7 +0,0 @@
-package ru.example.framework.constants;
-
-public class BrowserTypes {
-    public static final String CHROMIUM = "chromium";
-    public static final String WEBKIT = "webkit";
-    public static final String FIREFOX = "firefox";
-}
--- src/test/java/ru/example/projects/firstproject/FirstTest.java
+++ src/test/java/ru/example/projects/firstproject/FirstTest.java
@@ -12,23 +12,23 @@ package ru.example.projects.firstproject;
 
 import com.microsoft.playwright.Page;
 
+import com.microsoft.playwright.junit.UsePlaywright;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.example.framework.testclasses.BaseWebTest;
-
-import java.nio.file.Paths;
+import ru.example.framework.playwright.PWOptions;
 
 import static ru.example.framework.allure.AllureUtil.logToAllure;
 import static ru.example.framework.allure.AllureUtil.makeScreenshot;
 
+@UsePlaywright(PWOptions.class)
 @DisplayName("Первый suite")
-class FirstTest extends BaseWebTest {
+class FirstTest {
 
 	@Test
 	@DisplayName("1 : Тест первый")
 	@Description("Описание")
-	void simpleTest() {
+	void simpleTest(Page page) {
 		page.navigate("https://playwright.dev/");
 		//page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
 		logToAllure("just test step");
@@ -38,8 +38,22 @@ class FirstTest extends BaseWebTest {
 	@Test
 	@DisplayName("2 : Тест второй")
 	@Description("Описание")
-	void simpleTest2() {
+	void simpleTest2(Page page) {
 		page.navigate("https://playwright.dev/");
 		//page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+		//page.getByLabel("getStarted_Sjon").click();
+		logToAllure("just test step");
+		makeScreenshot(page,"1");
+	}
+
+	@Test
+	@DisplayName("3 : Тест третий")
+	@Description("Описание")
+	void simpleTest3(Page page) {
+		page.navigate("https://playwright.dev/");
+		//page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+		//page.getByLabel("getStarted_Sjon").click();
+		logToAllure("just test step");
+		makeScreenshot(page,"1");
 	}
 }
