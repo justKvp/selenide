a4b8342268b4fd576696f6930d64153f2e81abe0
kvp <kvipka90@yandex.ru>
2024-06-22 18:35:59 +0300

    remove pw 4

--- src/test/java/ru/example/framework/config/ConfigMgr.java
+++ src/test/java/ru/example/framework/config/ConfigMgr.java
@@ -1,10 +1,13 @@
 package ru.example.framework.config;
 
+import com.codeborne.selenide.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.util.Objects;
@@ -37,6 +40,17 @@ public class ConfigMgr {
         config = om.readValue(file, Config.class);
 
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        System.setProperty("selenide.browser", "Chrome");
+        Configuration.browserBinary = "/usr/bin/chromium-browser";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        //Configuration.headless = true;
+
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        Configuration.browserCapabilities = desiredCapabilities;
     }
 }
